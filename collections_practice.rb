99aa69ce1240819eedbc35ae96508275f541a8fc
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-12 02:02:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -53,13 +53,75 @@ def remove_non_strings(array)
 
 end # remove_non_strings
 
-def count_element(array)
+def count_elements(array)
 
- array.each_with_object(Hash.new(0)) do |item, hash|
-  hash[item] += 1
- end  
+#Note array is hash with same key values
+new_array = []
+hash_values = []
+word = ""
+  array.each do |hash_element|
+     hash_values <<  hash_element.values
+  end
+  hash_values2 = hash_values.flatten
+  #puts "hash_values2 = #{hash_values2}"
   
-end
+  element_uniq1 = hash_values.uniq
+  element_uniq = element_uniq1.flatten
+  #puts "element_uniq = #{element_uniq}"
+  
+  count1 = 0
+  element_uniq.each do |uniq_element|
+    hash_values2.each do |element|
+      if element == uniq_element
+        count1+=1
+      end
+      word = uniq_element
+    end
+    new_array << {name: word, count: count1}
+    count1 = 0
+  end
+  new_array
+  
+end #count_elements
+
+def merge_data(keys, data)
+ #Combines two nested data structures
+ # keys = [{:name=>"blake"}, {:name=>"blake"}, {:name=>"ashley"}]
+ # data = [{"blake"=>{:awesomeness=>10, :height=>"74", :last_name=>"johnson"}, "ashley"=>{:awesomeness=>9, :height=>60, :last_name=>"dubs"}}]
+ 
+ array = data[0].values.map.with_index {|x, i| keys[i].merge(x)}
+ array
+ 
+end # merge_data
+
+def find_cool(array)
+  # array = [{:name=>"ashley", :temperature=>"sort of cool"}, {:name=>"blake", :temperature=>"cool"}]
+  #output = [{:name=>"blake", :temperature=>"cool"}]
+  cool_array = []
+  array.each do |data|
+    data_values = data.values
+    if data_values.include?("cool")
+      cool_array << data
+      return cool_array
+    end
+  end
+end #find_cool
+
+def organize_schools(array)
+  
+   city_hash = {}
+  array.each do |type, data|
+    city_name = data.values
+    city = city_name[0]
+    if city_hash[city] != nil
+       city_hash[city] << type
+    else
+      city_hash[city] = [type]
+    end
+  end
+  city_hash
+  
+end # organize_schools
   
 
 
